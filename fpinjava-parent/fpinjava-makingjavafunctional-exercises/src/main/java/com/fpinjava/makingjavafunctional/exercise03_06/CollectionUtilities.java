e0dc85a9a51d78cd2a04cb00d4c7b93ead062229
Fredy Wijaya <fredy.wijaya@gmail.com>
2020-01-02 00:18:07 -0600

    Solve exercise 3.6

--- fpinjava-parent/fpinjava-makingjavafunctional-exercises/src/main/java/com/fpinjava/makingjavafunctional/exercise03_06/CollectionUtilities.java
+++ fpinjava-parent/fpinjava-makingjavafunctional-exercises/src/main/java/com/fpinjava/makingjavafunctional/exercise03_06/CollectionUtilities.java
@@ -49,7 +49,11 @@ public class CollectionUtilities {
   public static <T, U> U foldLeft(List<T> ts,
                                   U identity,
                                   Function<U, Function<T, U>> f) {
-    throw new RuntimeException("To be implemented");
+    U result = identity;
+    for (T t : ts) {
+      result = f.apply(result).apply(t);
+    }
+    return result;
   }
 
   public static <T> List<T> append(List<T> list, T t) {
