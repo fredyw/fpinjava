9c1a57aeb2d1f8c2f531ed30140a2e9defdb563f
Fredy Wijaya <fredy.wijaya@gmail.com>
2020-01-03 23:00:20 -0600

    Solve exercise 4.2

--- fpinjava-parent/fpinjava-recursion-exercises/src/main/java/com/fpinjava/recursion/exercise04_02/Fibonacci.java
+++ fpinjava-parent/fpinjava-recursion-exercises/src/main/java/com/fpinjava/recursion/exercise04_02/Fibonacci.java
@@ -1,11 +1,23 @@
 package com.fpinjava.recursion.exercise04_02;
 
+import com.fpinjava.common.TailCall;
+
 import java.math.BigInteger;
 
 
 public class Fibonacci {
 
   public static BigInteger fib(int x) {
-    throw new RuntimeException("To be implemented.");
+    return fibTail(BigInteger.valueOf(x), BigInteger.ONE, BigInteger.ZERO).eval();
+  }
+
+  private static TailCall<BigInteger> fibTail(BigInteger x, BigInteger accu1, BigInteger accu2) {
+    if (x.equals(BigInteger.ZERO)) {
+      return TailCall.ret(x);
+    }
+    if (x.equals(BigInteger.ONE)) {
+      return TailCall.ret(accu1.add(accu2));
+    }
+    return TailCall.sus(() -> fibTail(x.subtract(BigInteger.ONE), accu2, accu1.add(accu2)));
   }
 }
