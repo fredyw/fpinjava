619dd8a930d0da0c5d4d7fe75fe2164b5ad33320
Fredy Wijaya <fredy.wijaya@gmail.com>
2020-01-04 11:02:41 -0600

    Solve exercise 4.6

--- fpinjava-parent/fpinjava-recursion-exercises/src/main/java/com/fpinjava/recursion/exercise04_06/ComposeAll.java
+++ fpinjava-parent/fpinjava-recursion-exercises/src/main/java/com/fpinjava/recursion/exercise04_06/ComposeAll.java
@@ -2,12 +2,13 @@ package com.fpinjava.recursion.exercise04_06;
 
 import java.util.List;
 
+import com.fpinjava.common.CollectionUtilities;
 import com.fpinjava.common.Function;
 
 
 public class ComposeAll {
 
   static <T> Function<T, T> composeAll(List<Function<T, T>> list) {
-    throw new RuntimeException("To be implemented.");
+    return CollectionUtilities.foldRight(list, Function.identity(), x -> y -> x.compose(y));
   }
 }
