69acc2e17fb4f0a01d71de2f7b371af2ba15aa72
Fredy Wijaya <fredy.wijaya@gmail.com>
2020-01-03 23:10:45 -0600

    Solve exercise 4.3

--- fpinjava-parent/fpinjava-recursion-exercises/src/main/java/com/fpinjava/recursion/exercise04_03/FoldLeft.java
+++ fpinjava-parent/fpinjava-recursion-exercises/src/main/java/com/fpinjava/recursion/exercise04_03/FoldLeft.java
@@ -2,12 +2,25 @@ package com.fpinjava.recursion.exercise04_03;
 
 import java.util.List;
 
+import com.fpinjava.common.CollectionUtilities;
 import com.fpinjava.common.Function;
+import com.fpinjava.common.TailCall;
 
 
 public class FoldLeft {
 
   public static <T, U> U foldLeft(List<T> ts, U identity, Function<U, Function<T, U>> f) {
-    throw new RuntimeException("To be implemented.");
+    return foldLeftTail(ts, identity, f).eval();
+  }
+
+  private static <T, U> TailCall<U> foldLeftTail(List<T> ts, U identity, Function<U, Function<T, U>> f) {
+    if (ts.isEmpty()) {
+      return TailCall.ret(identity);
+    }
+    return TailCall.sus(
+        () -> foldLeftTail(
+            CollectionUtilities.tail(ts),
+            f.apply(identity).apply(CollectionUtilities.head(ts)),
+            f));
   }
 }
