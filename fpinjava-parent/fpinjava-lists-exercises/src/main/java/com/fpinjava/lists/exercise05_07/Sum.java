c45c7c38a88a1a0ee5aea793f04a8fd160d308e6
Fredy Wijaya <fredy.wijaya@gmail.com>
2020-03-01 00:41:39 -0600

    Solve exercise 5.7

--- fpinjava-parent/fpinjava-lists-exercises/src/main/java/com/fpinjava/lists/exercise05_07/Sum.java
+++ fpinjava-parent/fpinjava-lists-exercises/src/main/java/com/fpinjava/lists/exercise05_07/Sum.java
@@ -1,10 +1,18 @@
 package com.fpinjava.lists.exercise05_07;
 
+import com.fpinjava.common.TailCall;
 import com.fpinjava.lists.exercise05_06.List;
 
 public class Sum {
 
   public static Integer sum(List<Integer> ints) {
-    throw new RuntimeException("To be implemented");
+    return sum(ints, 0).eval();
+  }
+
+  private static TailCall<Integer> sum(List<Integer> ints, int acc) {
+    if (ints.isEmpty()) {
+      return TailCall.ret(acc);
+    }
+    return sum(ints.tail(), acc + ints.head());
   }
 }
