e45898a4f2928b1a37aa27b2c2d404be160ab774
Fredy Wijaya <fredy.wijaya@gmail.com>
2020-03-01 00:43:55 -0600

    Solve exercise 5.8

--- fpinjava-parent/fpinjava-lists-exercises/src/main/java/com/fpinjava/lists/exercise05_08/Product.java
+++ fpinjava-parent/fpinjava-lists-exercises/src/main/java/com/fpinjava/lists/exercise05_08/Product.java
@@ -1,10 +1,18 @@
 package com.fpinjava.lists.exercise05_08;
 
+import com.fpinjava.common.TailCall;
 import com.fpinjava.lists.exercise05_06.List;
 
 public class Product {
 
   public static Double product(List<Double> ints) {
-    throw new RuntimeException("To be implemented");
+    return product(ints, 1).eval();
+  }
+
+  private static TailCall<Double> product(List<Double> list, double acc) {
+    if (list.isEmpty()) {
+      return TailCall.ret(acc);
+    }
+    return product(list.tail(), list.head() * acc);
   }
 }
