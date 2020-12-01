fd50244a06331d6fb0bcf898fa6af5feb21c5b62
Fredy Wijaya <fredy.wijaya@gmail.com>
2020-11-30 21:58:54 -0600

    Solve exercise 9.18

--- fpinjava-parent/fpinjava-laziness-exercises/src/main/java/com/fpinjava/laziness/exercise09_18/Stream.java
+++ fpinjava-parent/fpinjava-laziness-exercises/src/main/java/com/fpinjava/laziness/exercise09_18/Stream.java
@@ -249,14 +249,16 @@ abstract class Stream<A> {
   }
 
   public static <A, S> Stream<A> unfold(S z, Function<S, Result<Tuple<A, S>>> f) {
-    throw new IllegalStateException("To be implemented");
+    return f.apply(z).map(x -> cons(() -> x._1,
+        () -> unfold(x._2, f))).getOrElse(empty());
   }
 
   public static Stream<Integer> from(int n) {
-    throw new IllegalStateException("To be implemented");
+    return unfold(n, x -> Result.success(new Tuple<>(x, x + 1)));
   }
 
   public static Stream<Integer> fibs() {
-    throw new IllegalStateException("To be implemented");
+    return unfold(new Tuple<>(1, 1),
+        x -> Result.success(new Tuple<>(x._1, new Tuple<>(x._2, x._1 + x._2))));
   }
 }
